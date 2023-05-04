f2cd3cf8e199a4cfd4d6c3cd8139f8034dce60f8
abhishekApplore <abhishek.verma@applore.in>
2023-05-04 16:09:52 +0530

    updates

--- src/routes/private/dispute/dispute.routes.js
+++ src/routes/private/dispute/dispute.routes.js
@@ -5,10 +5,20 @@ const AuthMiddleware = require("../../../middlewares/auth.middleware");
 
 const router = require("express").Router();
 
-router.use(AsyncHandler(AuthMiddleware));
-
-router.get("/", AsyncHandler(dispute.getAllDisputes));
-router.post("/raise/:id", AsyncHandler(dispute.createDispute));
-router.patch("/update-status/:id", AsyncHandler(dispute.updatesDisputeStatus));
+router.get(
+  "/",
+  AsyncHandler(AuthMiddleware),
+  AsyncHandler(dispute.getAllDisputes)
+);
+router.post(
+  "/raise/:id",
+  AsyncHandler(AuthMiddleware),
+  AsyncHandler(dispute.createDispute)
+);
+router.patch(
+  "/update-status/:id",
+  AsyncHandler(AuthMiddleware),
+  AsyncHandler(dispute.updatesDisputeStatus)
+);
 
 module.exports = router;
