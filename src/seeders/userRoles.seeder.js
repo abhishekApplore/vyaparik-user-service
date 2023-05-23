05fa017ee4dba4fdbfbe19fd7accbcca02866311
abhishekApplore <abhishek.verma@applore.in>
2023-05-23 16:46:28 +0530

    updates

--- src/seeders/userRoles.seeder.js
+++ src/seeders/userRoles.seeder.js
@@ -3,12 +3,30 @@ const permissionData = require("../data/permissions.json");
 const roleData = require("../data/roles.json");
 const UserRoleService = require("../services/userRole.service");
 const logger = require("../helpers/logger");
+const user = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 // Seeds Roles and Permissions
 const UserRoleSeeder = async () => {
   logger.info("Seeding User Roles and Permissions...");
 
   try {
+    bcrypt.hash("123456", 10, async function (err, hash) {
+      await user.findOneAndUpdate(
+        {
+          email: "admin@vyaparik.com",
+        },
+        {
+          email: "admin@vyaparik.com",
+          password: hash,
+          type: "ADMIN",
+        },
+        {
+          upsert: true,
+        }
+      );
+    });
+
     const promises = [];
 
     if (!(await UserPermissionService.exists(permissionData[0]._id))) {
