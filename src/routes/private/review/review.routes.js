a36d9e91887ab48cd662a82e645f11f60cb626e4
abhishekApplore <abhishek.verma@applore.in>
2023-05-26 11:27:00 +0530

    updaets

--- src/routes/private/review/review.routes.js
+++ src/routes/private/review/review.routes.js
@@ -1,12 +1,15 @@
-const { addReview, getReviewsOfParticularUser, getReviewsOfParticularSeller } = require("../../../controllers/review.controller");
+const {
+  addReview,
+  getReviewsOfParticularUser,
+  getReviewsOfParticularSeller,
+} = require("../../../controllers/review.controller");
 const { AsyncHandler } = require("../../../helpers/utils.helper");
+const Auth = require("../../../middlewares/auth.middleware");
 
 const router = require("express").Router();
 
-router.post("/create",AsyncHandler(addReview));
-router.get("/:id",AsyncHandler(getReviewsOfParticularUser));
-router.get("/seller/:id",AsyncHandler(getReviewsOfParticularSeller));
-
-
+router.post("/create", AsyncHandler(Auth), AsyncHandler(addReview));
+router.get("/:id", AsyncHandler(getReviewsOfParticularUser));
+router.get("/seller/:id", AsyncHandler(getReviewsOfParticularSeller));
 
 module.exports = router;
